72befaf155f1cecfb5035a03b7de95056cab0161
Franco Natanael Tomaino <tomainofranco5@gmail.com>
2025-10-06 12:49:36 -0300

    esLint configuration

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  extends: [
+    'next',
+    'next/core-web-vitals',
+    'prettier'
+  ],
 };
 
 export default nextConfig;
